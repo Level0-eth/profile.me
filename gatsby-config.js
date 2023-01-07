3a316415544e7c7ad6bd9bd438b1bc21d581cad3
shubham44-code <shubham162001@gmail.com>
2023-01-07 18:37:48 +0530

    lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,21 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+/* eslint-disable no-undef */
 module.exports = {
   siteMetadata: {
     title: `profile.me`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/"
+      },
+      __key: "pages"
+    }
+  ]
 };
