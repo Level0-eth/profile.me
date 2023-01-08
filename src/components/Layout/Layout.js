17f52159978f8e198fbe6b9fd5337b40bc8ff40c
shubham44-code <shubham162001@gmail.com>
2023-01-08 15:33:50 +0530

    Layout component added

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "../../../static/css/reset.css";
+import "../../../static/css/global.css";
+
+const Layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Layout;
+
+Layout.propTypes = {
+  children: PropTypes.object
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+import Layout from "../components/Layout/Layout";
+
 const IndexPage = () => {
   return (
-    <>
+    <Layout>
       <Helmet>
         <title>Home</title>
         <link rel="icon" type="image/x-icon" href="./images/logo.png" />
       </Helmet>
-    </>
+    </Layout>
   );
 };
 
